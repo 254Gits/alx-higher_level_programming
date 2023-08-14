df4a7adfc79bf1e5c7e9aabf4044c85a9389f99a
254Gits <emmanuelkirimi2016@gmail.com>
2023-08-14 19:15:21 +0000

    replacing algorithim

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,5 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     my_list[idx] = element
-    for idx in my_list:
+    for idx in range(len(my_list)):
         return my_list
