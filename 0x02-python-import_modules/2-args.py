6adffe2dd99b0d9d8ee08235f2f5f1537fb14e7f
254Gits <emmanuelkirimi2016@gmail.com>
2023-08-16 21:11:44 +0000

    calculator

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
 
     num = len(sys.argv)
 
-    if num == 2:
-        print("{}argument.".format(num - 1))
-    elif num == 1:
-        print ("{} arguments:".format(num -1))
+    if num == 1:
+        print("{}arguments.".format(num - 1))
+    elif num == 2:
+        print ("{} argument:".format(num -1))
     else:
         print("{}arguments:".format(num - 1))
 
